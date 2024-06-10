4f12d30e0fa45b5da5844ad931e146c1a799aa5b
Igor Fedorenko <ifedorenko@salesforce.com>
2024-06-10 08:32:33 -0400

    DynamoDb-local 2.5.1 NPE
    Signed-off-by: Igor Fedorenko <ifedorenko@salesforce.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Demonstrates unexpected NullPointerException when using DynamoDb-local 2.5.0. (latest as of 2024-06-01).
+Demonstrates unexpected NullPointerException when using DynamoDb-local 2.5.1. (latest as of 2024-06-09).
 Works as expected with DynamoDbLocal 2.2.1.
 
 Steps to reproduce the problem
@@ -10,18 +10,17 @@ Example `adhoc.dynamodb_local.DDBLocalEmbeddedBillingMode` output:
 SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
 SLF4J: Defaulting to no-operation (NOP) logger implementation
 SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
-createTable SHARED.jobs
-createTable testtable
-Exception in thread "main" java.lang.NullPointerException: Cannot invoke "com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputDescription.getReadCapacityUnits()" because the return value of "com.amazonaws.services.dynamodbv2.model.GlobalSecondaryIndexDescription.getProvisionedThroughput()" is null
-	at com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.ControlPlaneFunction.validateProvisionedThroughputWithGSIs(ControlPlaneFunction.java:668)
-	at com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.CreateTableFunction.validateProvisionedThroughput(CreateTableFunction.java:172)
-	at com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.CreateTableFunction.apply(CreateTableFunction.java:122)
-	at com.amazonaws.services.dynamodbv2.local.shared.access.awssdkv1.client.LocalAmazonDynamoDB.createTable(LocalAmazonDynamoDB.java:229)
+createTable SHARED.data.clouds.c2.0.shared.Objects
+updateTable SHARED.data.clouds.c2.0.shared.Objects
+Exception in thread "main" java.lang.NullPointerException: Cannot invoke "com.amazonaws.services.dynamodbv2.model.ProvisionedThroughputDescription.getLastIncreaseDateTime()" because the return value of "com.amazonaws.services.dynamodbv2.model.GlobalSecondaryIndexDescription.getProvisionedThroughput()" is null
+	at com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.UpdateTableFunction$1.criticalSection(UpdateTableFunction.java:219)
+	at com.amazonaws.services.dynamodbv2.local.shared.access.LocalDBAccess$WriteLockWithTimeout.execute(LocalDBAccess.java:388)
+	at com.amazonaws.services.dynamodbv2.local.shared.access.api.cp.UpdateTableFunction.apply(UpdateTableFunction.java:341)
+	at com.amazonaws.services.dynamodbv2.local.shared.access.awssdkv1.client.LocalAmazonDynamoDB.updateTable(LocalAmazonDynamoDB.java:319)
 	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
 	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:77)
 	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
 	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
 	at com.amazonaws.services.dynamodbv2.local.embedded.DDBExceptionMappingInvocationHandler.invoke(DDBExceptionMappingInvocationHandler.java:173)
-	at jdk.proxy1/jdk.proxy1.$Proxy22.createTable(Unknown Source)
-	at adhoc.dynamodb_local.DDBLocalEmbeddedBillingMode.main(DDBLocalEmbeddedBillingMode.java:28)
-```
+	at jdk.proxy1/jdk.proxy1.$Proxy22.updateTable(Unknown Source)
+	at adhoc.dynamodb_local.DDBLocalEmbeddedBillingMode.main(DDBLocalEmbeddedBillingMode.java:36)```
--- src/main/java/adhoc/dynamodb_local/DDBLocalEmbeddedBillingMode.java
+++ src/main/java/adhoc/dynamodb_local/DDBLocalEmbeddedBillingMode.java
@@ -4,7 +4,7 @@ import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
 import com.amazonaws.services.dynamodbv2.model.*;
 
 /**
- * Demonstrates unexpected NullPointerException when using DynamoDb-local 2.5.0.
+ * Demonstrates unexpected NullPointerException when using DynamoDb-local 2.5.1.
  * Works as expected with DynamoDb-local 2.2.1
  */
 public class DDBLocalEmbeddedBillingMode {
@@ -12,27 +12,46 @@ public class DDBLocalEmbeddedBillingMode {
         var server = DynamoDBEmbedded.create();
         var db = server.amazonDynamoDB();
 
-        System.out.println("createTable SHARED.jobs");
+        System.out.println("createTable SHARED.data.clouds.c2.0.shared.Objects");
         db.createTable(
                 new CreateTableRequest()
-                        .withTableName("SHARED.jobs")
-                        .withAttributeDefinitions(new AttributeDefinition("hk", ScalarAttributeType.B), new AttributeDefinition("sk", ScalarAttributeType.B), new AttributeDefinition("outstanding_tasks_hk", ScalarAttributeType.B), new AttributeDefinition("execute_after", ScalarAttributeType.B))
-                        .withKeySchema(new KeySchemaElement("hk", KeyType.HASH), new KeySchemaElement("sk", KeyType.RANGE))
+                        .withTableName("SHARED.data.clouds.c2.0.shared.Objects")
+                        .withAttributeDefinitions(new AttributeDefinition("0", ScalarAttributeType.B), new AttributeDefinition("1", ScalarAttributeType.B), new AttributeDefinition("4", ScalarAttributeType.B), new AttributeDefinition("5", ScalarAttributeType.B), new AttributeDefinition("2", ScalarAttributeType.B), new AttributeDefinition("3", ScalarAttributeType.B), new AttributeDefinition("2A", ScalarAttributeType.B), new AttributeDefinition("3A", ScalarAttributeType.B))
+                        .withKeySchema(new KeySchemaElement("0", KeyType.HASH), new KeySchemaElement("1", KeyType.RANGE))
                         .withBillingMode(BillingMode.PAY_PER_REQUEST)
                         .withGlobalSecondaryIndexes(new GlobalSecondaryIndex()
-                                .withIndexName("outstanding_tasks")
-                                .withKeySchema(new KeySchemaElement("outstanding_tasks_hk", KeyType.HASH), new KeySchemaElement("execute_after", KeyType.RANGE))
+                                .withIndexName("idx_0_1")
+                                .withKeySchema(new KeySchemaElement("4", KeyType.HASH), new KeySchemaElement("5", KeyType.RANGE))
+                                .withProjection(new Projection().withProjectionType(ProjectionType.ALL)), new GlobalSecondaryIndex()
+                                .withIndexName("idx_0_0")
+                                .withKeySchema(new KeySchemaElement("2", KeyType.HASH), new KeySchemaElement("3", KeyType.RANGE))
+                                .withProjection(new Projection().withProjectionType(ProjectionType.ALL)), new GlobalSecondaryIndex()
+                                .withIndexName("idx_0_2")
+                                .withKeySchema(new KeySchemaElement("2A", KeyType.HASH), new KeySchemaElement("3A", KeyType.RANGE))
                                 .withProjection(new Projection().withProjectionType(ProjectionType.ALL)))
+                        .withStreamSpecification(new StreamSpecification().withStreamEnabled(true).withStreamViewType(StreamViewType.NEW_AND_OLD_IMAGES))
         );
 
-        System.out.println("createTable testtable");
-        db.createTable(
-                new CreateTableRequest()
-                        .withTableName("testtable")
-                        .withAttributeDefinitions(new AttributeDefinition("hk", ScalarAttributeType.S))
-                        .withKeySchema(new KeySchemaElement("hk", KeyType.HASH))
+        System.out.println("updateTable SHARED.data.clouds.c2.0.shared.Objects");
+        db.updateTable(
+                new UpdateTableRequest()
+                        .withTableName("SHARED.data.clouds.c2.0.shared.Objects")
                         .withBillingMode(BillingMode.PROVISIONED)
-                        .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L))
+                        .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(20000L).withWriteCapacityUnits(20000L))
+                        .withGlobalSecondaryIndexUpdates(
+                                new GlobalSecondaryIndexUpdate().withUpdate(
+                                        new UpdateGlobalSecondaryIndexAction()
+                                                .withIndexName("idx_0_0")
+                                                .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(20000L).withWriteCapacityUnits(20000L))),
+                                new GlobalSecondaryIndexUpdate().withUpdate(
+                                        new UpdateGlobalSecondaryIndexAction()
+                                                .withIndexName("idx_0_2")
+                                                .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(20000L).withWriteCapacityUnits(20000L))),
+                                new GlobalSecondaryIndexUpdate().withUpdate(
+                                        new UpdateGlobalSecondaryIndexAction()
+                                                .withIndexName("idx_0_1")
+                                                .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(20000L).withWriteCapacityUnits(20000L)))
+                        )
         );
 
         server.shutdownNow();
